13a612a969c4bd6da042552429b4a23f0c95b6de
Aswin8809 <aswinprasanth00@gmail.com>
2026-03-25 17:29:57 +0530

    Add files via upload

---
+++ AboutUs.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Student Performance Tracker</h1>
+      <p>
+        This project helps track student academic performance by calculating
+        total marks, average, and grades.
+      </p>
+
+      <h3>Features:</h3>
+      <ul>
+        <li>Input student marks</li>
+        <li>Calculate average</li>
+        <li>Display grade</li>
+      </ul>
+
+      <h3>Technologies Used:</h3>
+      <p>Python / JavaScript</p>
+    </div>
+  );
+}
+
+export default AboutUs;
